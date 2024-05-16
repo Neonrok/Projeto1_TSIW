b9ec39bcfc699460f5b0b9802bc0044b84d14761
Xavier Kooijman <xavierkooijman@gmail.com>
2024-05-16 23:42:12 +0100

    NavLink hover underline animation, class based css for reusability

--- js/views/headerView.js
+++ js/views/headerView.js
@@ -1,17 +1,19 @@
 export function createHeader(){
   const header = document.createElement("header")
   header.innerHTML = `
-  <a href="index.html"><img src="./img/LogoTSIW.svg"></a>
-  <nav class="navbar">
-      <ul class="navbarLinks">
-          <li><a href="" class="navLink text-16 text-medium">Aprender</a></li>
-          <li><a href="" class="navLink text-16 text-medium">Atividades</a></li>
-          <li><a href="" class="navLink text-16 text-medium">Projetos</a></li>
-          <li><a href="" class="navLink text-16 text-medium">Testemunhos</a></li>
-          <li><a href="" class="navLink text-16 text-medium">Sobre</a></li>
-      </ul>
-      <a href="" class="button" role="button">Registar</a>
-  </nav>`
+  <header class="header flex items-center w-100 px-container">
+    <a href="index.html"><img src="./img/LogoTSIW.svg"></a>
+    <nav class="flex items-center gap-32">
+        <ul class="flex items-center gap-24">
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Aprender</a></li>
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Atividades</a></li>
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Projetos</a></li>
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Testemunhos</a></li>
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Sobre</a></li>
+        </ul>
+        <a href="" class="button" role="button">Registar</a>
+    </nav>
+  </header>`
   const mainContainer = document.getElementById("mainContainer")
   mainContainer.prepend(header) 
 }
