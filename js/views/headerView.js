e8cf17f140baccbe95330ff967295ccc0531daaf
Xavier Kooijman <xavierkooijman@gmail.com>
2024-05-19 00:53:04 +0100

    added responsiveness for large screens, flexbox to hero section and set max-width for hero description.

--- js/views/headerView.js
+++ js/views/headerView.js
@@ -1,18 +1,20 @@
 export function createHeader(){
   const header = document.createElement("header")
   header.innerHTML = `
-  <header class="header flex items-center w-100 px-container">
-    <a href="index.html"><img src="./img/LogoTSIW.svg"></a>
-    <nav class="flex items-center gap-32">
-        <ul class="flex items-center gap-24">
-            <li><a href="" class="hover-underline-animation text-16 text-medium">Aprender</a></li>
-            <li><a href="" class="hover-underline-animation text-16 text-medium">Atividades</a></li>
-            <li><a href="" class="hover-underline-animation text-16 text-medium">Projetos</a></li>
-            <li><a href="" class="hover-underline-animation text-16 text-medium">Testemunhos</a></li>
-            <li><a href="" class="hover-underline-animation text-16 text-medium">Sobre</a></li>
-        </ul>
-        <a href="" class="button" role="button">Registar</a>
-    </nav>
+  <header class="border-bottom">
+    <div class="header flex space-between items-center container mx-auto px-container">
+      <a href="index.html"><img src="./img/LogoTSIW.svg"></a>
+      <nav class="flex items-center gap-32">
+          <ul class="flex items-center gap-24">
+              <li><a href="" class="hover-underline-animation text-16 text-medium">Aprender</a></li>
+              <li><a href="" class="hover-underline-animation text-16 text-medium">Atividades</a></li>
+              <li><a href="" class="hover-underline-animation text-16 text-medium">Projetos</a></li>
+              <li><a href="" class="hover-underline-animation text-16 text-medium">Testemunhos</a></li>
+              <li><a href="" class="hover-underline-animation text-16 text-medium">Sobre</a></li>
+          </ul>
+          <a href="" class="button" role="button">Registar</a>
+      </nav>
+    </div>  
   </header>`
   const mainContainer = document.getElementById("mainContainer")
   mainContainer.prepend(header) 
