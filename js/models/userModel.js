bf10dcc3d203c2b35e6ec7914c7a31e4923f6b86
Xavier Kooijman <xavierkooijman@gmail.com>
2024-05-27 00:32:06 +0100

    função para verificar login e adicionar loggedUser ao sessionStorage

--- js/models/userModel.js
+++ js/models/userModel.js
@@ -2,11 +2,22 @@ let users = []
 
 export function addUser(name, password) {
   if (users.some((user) => user.name === name)) {
-    throw Error(`Utilizador com o nome "${user.name}" já existe`);
+    throw Error(`Utilizador com o nome "${user.name}" já existe`)
   } 
   else{
-    users.push(new User(name, password));
-    localStorage.setItem("users", JSON.stringify(users));
+    users.push(new User(name, password))
+    localStorage.setItem("users", JSON.stringify(users))
+  }
+}
+
+export function login(name,password){
+  const user = users.find((user) => user.name === name && user.password === password);
+  if (user) {
+      sessionStorage.setItem("loggedUser", JSON.stringify(user));
+      return true;
+  } 
+  else{
+    throw Error("Login Inválido");
   }
 }
 
