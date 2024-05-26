aa3d9525e3c5e1a8fdcffeae8555f8d079d213fb
Xavier Kooijman <xavierkooijman@gmail.com>
2024-05-26 22:32:49 +0100

    atividadeModel class atividades função para adicionar novas atividades

---
+++ js/models/atividadeModel.js
@@ -0,0 +1,21 @@
+let atividades = []
+
+export function addAtividade(title,image,date,description){
+  if(atividades.some((atividade) => atividade.title === title)){
+    throw Error(`Atividade com o título ${title} já existe`)
+  }
+  else{
+    atividades.push(new Atividade(title,image,date,description))
+    localStorage.setItem("atividades", JSON.stringify(atividades))
+  }
+}
+
+class Atividade{
+  constructor(title,image,date,description){
+    this.title = title
+    this.image = image
+    this.date = date
+    this.description = description
+  }
+
+}
