1abbaf329a3cece9ed0ad6c7199173f143c5332f
Xavier Kooijman <xavierkooijman@gmail.com>
2024-05-28 00:32:58 +0100

    sideNavBar correct placing and width, z-index:50 to be above other page elements and using small logo version

--- js/views/headerView.js
+++ js/views/headerView.js
@@ -2,20 +2,56 @@ export function createHeader(){
   const header = document.createElement("header")
   header.innerHTML = `
   <header class="border-bottom">
-    <div class="header flex space-between items-center container mx-auto px-container">
-      <a href="../index.html"><img src="/img/LogoTSIW.svg"></a>
-      <nav class="flex items-center gap-32">
-          <ul class="flex items-center gap-24">
-              <li><a href="" class="hover-underline-animation text-16 text-medium">Aprender</a></li>
-              <li><a href="" class="hover-underline-animation text-16 text-medium">Atividades</a></li>
-              <li><a href="" class="hover-underline-animation text-16 text-medium">Projetos</a></li>
-              <li><a href="" class="hover-underline-animation text-16 text-medium">Testemunhos</a></li>
-              <li><a href="" class="hover-underline-animation text-16 text-medium">Sobre</a></li>
+  <div class="header flex space-between items-center container mx-auto px-container">
+    <a href="../index.html"><img src="/img/LogoTSIW.svg"></a>
+    <nav class="flex items-center gap-32">
+        <ul class="flex items-center gap-24">
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Aprender</a></li>
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Atividades</a></li>
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Projetos</a></li>
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Testemunhos</a></li>
+            <li><a href="" class="hover-underline-animation text-16 text-medium">Sobre</a></li>
+        </ul>
+        <a href="/html/registo.html" class="button" role="button">Registar</a>
+        <button class="header-hamburger flex direction-column">
+          <div class="hamburger-bar"></div>
+          <div class="hamburger-bar"></div>
+          <div class="hamburger-bar"></div>
+        </button>
+    </nav>
+  </div>
+  <div class="side-nav h-screen flex direction-column gap-48">
+      <div class="flex space-between items-center">
+          <a href="../index.html"><img src="/img/LogoTSIW Versao pequena.svg" height="30px"></a>
+          <div class="flex gap-16 items-center">
+              <a href="/html/registo.html" class="button" role="button">Registar</a>
+              <button class="header-hamburger flex direction-column">
+                <div class="hamburger-bar"></div>
+                <div class="hamburger-bar"></div>
+                <div class="hamburger-bar"></div>
+              </button>
+          </div>
+      </div>
+      <div class="flex direction-column gap-16">
+          <p class="text-18 text-medium text-mainGrey">Navegação</p>
+          <ul class="flex direction-column gap-16">
+              <li><a href="" class="hover-underline-animation text-18 text-medium">Aprender</a></li>
+              <li><a href="" class="hover-underline-animation text-18 text-medium">Atividades</a></li>
+              <li><a href="" class="hover-underline-animation text-18 text-medium">Projetos</a></li>
+              <li><a href="" class="hover-underline-animation text-18 text-medium">Testemunhos</a></li>
+              <li><a href="" class="hover-underline-animation text-18 text-medium">Sobre</a></li>
           </ul>
-          <a href="/html/registo.html" class="button" role="button">Registar</a>
-      </nav>
-    </div>  
-  </header>`
+      </div>
+      <div class="flex direction-column gap-16">
+          <p class="text-18 text-medium text-mainGrey">Redes Sociais</p>
+          <ul class="flex direction-column gap-16">
+              <li><a href="" class="hover-underline-animation text-18 text-medium">Facebook</a></li>
+              <li><a href="" class="hover-underline-animation text-18 text-medium">Instagram</a></li>
+              <li><a href="" class="hover-underline-animation text-18 text-medium">Youtube</a></li>
+          </ul>
+      </div>
+  </div>  
+</header>`
   const mainContainer = document.getElementById("mainContainer")
   mainContainer.prepend(header) 
 }
