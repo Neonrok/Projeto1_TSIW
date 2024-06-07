c1c5346ab17f2c1f58c0bcea6f78095eb905df68
Xavier Kooijman <xavierkooijman@gmail.com>
2024-06-07 01:51:00 +0100

    projeto slider view javascript, mudanças no css do slider

---
+++ js/views/projetoSliderView.js
@@ -0,0 +1,50 @@
+export function createSlider(projeto){
+  let currentPosition = 0
+  let projetoSlider = document.createElement("div")
+  projetoSlider.classList.add("projeto-slider")
+  let buttonsContainer = document.createElement("div")
+  buttonsContainer.classList.add("flex space-between container slider-buttons")
+  let sliderImages = projeto.images
+  let lastIndex = sliderImages.length - 1
+  
+  let sliderCurrentImage = document.createElement("img")
+  let leftButton = document.createElement("img")
+  let rightButton = document.createElement("img")
+  
+  sliderCurrentImage.src = sliderImages[0]
+  sliderCurrentImage.classList.add("projeto-img")
+  leftButton.src = "/img/Slider left button.svg"
+  rightButton.src = "/img/Slider right button.svg"
+
+  leftButton.addEventListener("click", () =>{
+    if(currentPosition == 0){
+      sliderCurrentImage.src = sliderImages[lastIndex]
+      currentPosition = lastIndex
+    }
+    else{
+      sliderCurrentImage.src = sliderImages[currentPosition - 1]
+      currentPosition = currentPosition - 1
+    }
+  })
+
+  rightButton.addEventListener("click", () =>{
+    if(currentPosition == lastIndex){
+      sliderCurrentImage.src = sliderImages[0]
+      currentPosition = 0
+    }
+    else{
+      sliderCurrentImage.src = sliderImages[currentPosition + 1]
+      currentPosition = currentPosition + 1
+    }
+  })
+
+  return `
+  <div class="projeto-slider">
+    <div class="flex space-between container slider-buttons">
+      ${leftButton}
+      <${rightButton}
+    </div>
+    ${sliderCurrentImage}
+  </div>
+  `
+}
