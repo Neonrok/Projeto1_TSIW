ac32631ea3ab462b0191d9e2d44b3fbd5cdf64b8
Xavier Kooijman <xavierkooijman@gmail.com>
2024-06-09 01:32:01 +0100

    userModel default avatar data url, default notifications, pagina conta html e css base

--- js/models/userModel.js
+++ js/models/userModel.js
@@ -36,9 +36,9 @@ export function isLogged() {
 
 class User{
 
-  avatar = ""
+  avatar = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPG1hc2sgaWQ9Im1hc2swXzEzNDJfMTAzNTgiIHN0eWxlPSJtYXNrLXR5cGU6YWxwaGEiIG1hc2tVbml0cz0idXNlclNwYWNlT25Vc2UiIHg9IjAiIHk9IjAiIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCI+CjxjaXJjbGUgY3g9IjMyIiBjeT0iMzIiIHI9IjMyIiBmaWxsPSIjRURFREVEIi8+CjwvbWFzaz4KPGcgbWFzaz0idXJsKCNtYXNrMF8xMzQyXzEwMzU4KSI+CjxjaXJjbGUgY3g9IjMyIiBjeT0iMzIiIHI9IjMyIiBmaWxsPSIjRURFREVEIi8+CjxlbGxpcHNlIGN4PSIzMi4wMDAxIiBjeT0iNjIuMjIyMyIgcng9IjMwLjIyMjIiIHJ5PSIyNi42NjY3IiBmaWxsPSIjQTBBMEEyIi8+CjxjaXJjbGUgY3g9IjMyLjAwMDIiIGN5PSIyMS4zMzMyIiByPSIxMC42NjY3IiBmaWxsPSIjQTBBMEEyIi8+CjwvZz4KPC9zdmc+Cg=="
   savedActivities = []
-  activeNotifications = []
+  activeNotifications = ["Atividades","Projetos","Próximas atividades","Desafios"]
   solvedChallenges = []
 
   constructor(name,password){
--- js/views/registoView.js
+++ js/views/registoView.js
@@ -4,7 +4,8 @@ const registoName = document.getElementById("registoName")
 const registoPassword = document.getElementById("registoPassword")
 const registoForm = document.getElementById("registoForm")
 
-registoForm.addEventListener("submit", ()=>{
+registoForm.addEventListener("submit", (event)=>{
+  event.preventDefault()
   User.addUser(registoName.value,registoPassword.value)
   User.login(registoName.value,registoPassword.value)
 })
