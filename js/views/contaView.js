2f5e97168b196ba2247acdd84b32998554246fed
Xavier Kooijman <xavierkooijman@gmail.com>
2024-06-15 18:58:19 +0100

    conta page navigate between sections done

--- js/views/contaView.js
+++ js/views/contaView.js
@@ -4,12 +4,24 @@ const atividadesGuardNavbutton = document.getElementById("atividadesGuardNavbutt
 const notifNavbutton = document.getElementById("notifNavbutton")
 const eliminarContaNavbutton = document.getElementById("eliminarContaNavbutton")
 
+const perfilSection = document.getElementById("perfilSection")
+const passwordSection = document.getElementById("passwordSection")
+const atividadesGuardSection = document.getElementById("atividadesGuardSection")
+const notifSection = document.getElementById("notifSection")
+const eliminarContaSection = document.getElementById("eliminarContaSection")
+
 function resetSectionSelection(){
   perfilNavbutton.classList.remove("active")
   passwordNavbutton.classList.remove("active")
   atividadesGuardNavbutton.classList.remove("active")
   notifNavbutton.classList.remove("active")
   eliminarContaNavbutton.classList.remove("active")
+
+  perfilSection.classList.add("d-none")
+  passwordSection.classList.add("d-none")
+  atividadesGuardSection.classList.add("d-none")
+  notifSection.classList.add("d-none")
+  eliminarContaSection.classList.add("d-none")
 }
 
 function activateSectionSelection(selection){
@@ -19,21 +31,25 @@ function activateSectionSelection(selection){
 
 perfilNavbutton.addEventListener("click", ()=>{
   activateSectionSelection(perfilNavbutton)
+  perfilSection.classList.remove("d-none")
 })
 
 passwordNavbutton.addEventListener("click", ()=>{
   activateSectionSelection(passwordNavbutton)
+  passwordSection.classList.remove("d-none")
 })
 
 atividadesGuardNavbutton.addEventListener("click", ()=>{
   activateSectionSelection(atividadesGuardNavbutton)
+  atividadesGuardSection.classList.remove("d-none")
 })
 
 notifNavbutton.addEventListener("click", ()=>{
   activateSectionSelection(notifNavbutton)
+  notifSection.classList.remove("d-none")
 })
 
 eliminarContaNavbutton.addEventListener("click", ()=>{
   activateSectionSelection(eliminarContaNavbutton)
+  eliminarContaSection.classList.remove("d-none")
 })
-
