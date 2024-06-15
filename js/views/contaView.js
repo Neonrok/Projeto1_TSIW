22bc83dcadd729a81fbfa0284384804df41f1283
Xavier Kooijman <xavierkooijman@gmail.com>
2024-06-15 18:38:31 +0100

    conta page, eliminar conta section html e css, javascript ativar e desativar estado ativo de seleção de sections

--- js/views/contaView.js
+++ js/views/contaView.js
@@ -3,3 +3,37 @@ const passwordNavbutton = document.getElementById("passwordNavbutton")
 const atividadesGuardNavbutton = document.getElementById("atividadesGuardNavbutton")
 const notifNavbutton = document.getElementById("notifNavbutton")
 const eliminarContaNavbutton = document.getElementById("eliminarContaNavbutton")
+
+function resetSectionSelection(){
+  perfilNavbutton.classList.remove("active")
+  passwordNavbutton.classList.remove("active")
+  atividadesGuardNavbutton.classList.remove("active")
+  notifNavbutton.classList.remove("active")
+  eliminarContaNavbutton.classList.remove("active")
+}
+
+function activateSectionSelection(selection){
+  resetSectionSelection()
+  selection.classList.add("active")
+}
+
+perfilNavbutton.addEventListener("click", ()=>{
+  activateSectionSelection(perfilNavbutton)
+})
+
+passwordNavbutton.addEventListener("click", ()=>{
+  activateSectionSelection(passwordNavbutton)
+})
+
+atividadesGuardNavbutton.addEventListener("click", ()=>{
+  activateSectionSelection(atividadesGuardNavbutton)
+})
+
+notifNavbutton.addEventListener("click", ()=>{
+  activateSectionSelection(notifNavbutton)
+})
+
+eliminarContaNavbutton.addEventListener("click", ()=>{
+  activateSectionSelection(eliminarContaNavbutton)
+})
+
