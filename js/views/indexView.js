f9782a41aad56443512e083828cd605b00847d19
Xavier Kooijman <xavierkooijman@gmail.com>
2024-06-15 23:15:41 +0100

    atividades section na homepage

---
+++ js/views/indexView.js
@@ -0,0 +1,36 @@
+import * as Atividade from "../models/atividadeModel.js"
+
+const atividadesContainer = document.getElementById("atividadesContainer")
+
+function renderAtividades(){
+  Atividade.init()
+  let atividades = Atividade.getAtividades()
+
+  for(let i=0;i<4;i++) {
+    atividadesContainer.append(generateAtividadeCard(atividades[i]))
+  }
+}
+
+function generateAtividadeCard(atividade){
+
+  let cardContainer = document.createElement("div")
+  cardContainer.className = "flex direction-column gap-16 atividade-card"
+
+  let cardImg = document.createElement("img")
+  cardImg.className = "atividade-img"
+  cardImg.src = atividade.image
+
+  let cardTitle = document.createElement("p")
+  cardTitle.className = "text-16 text-medium atividade-title"
+  cardTitle.innerText = atividade.title
+
+  cardContainer.append(cardImg,cardTitle)
+  cardContainer.addEventListener("click", ()=>{
+    Atividade.setCurrentAtividade(atividade.title)
+    location.href = "/html/atividade.html"
+  })
+
+  return cardContainer
+}
+
+renderAtividades()
