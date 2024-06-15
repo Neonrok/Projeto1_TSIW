9e82d1d665ecb425d381ad53b5448a636e64ce55
Xavier Kooijman <xavierkooijman@gmail.com>
2024-06-15 19:31:26 +0100

    change header based on if a user is logged or not

--- js/models/userModel.js
+++ js/models/userModel.js
@@ -34,6 +34,10 @@ export function isLogged() {
   return sessionStorage.getItem("loggedUser") ? true : false;
 }
 
+export function getUserLogged() {
+  return JSON.parse(sessionStorage.getItem("loggedUser"));
+}
+
 class User{
 
   avatar = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPG1hc2sgaWQ9Im1hc2swXzEzNDJfMTAzNTgiIHN0eWxlPSJtYXNrLXR5cGU6YWxwaGEiIG1hc2tVbml0cz0idXNlclNwYWNlT25Vc2UiIHg9IjAiIHk9IjAiIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCI+CjxjaXJjbGUgY3g9IjMyIiBjeT0iMzIiIHI9IjMyIiBmaWxsPSIjRURFREVEIi8+CjwvbWFzaz4KPGcgbWFzaz0idXJsKCNtYXNrMF8xMzQyXzEwMzU4KSI+CjxjaXJjbGUgY3g9IjMyIiBjeT0iMzIiIHI9IjMyIiBmaWxsPSIjRURFREVEIi8+CjxlbGxpcHNlIGN4PSIzMi4wMDAxIiBjeT0iNjIuMjIyMyIgcng9IjMwLjIyMjIiIHJ5PSIyNi42NjY3IiBmaWxsPSIjQTBBMEEyIi8+CjxjaXJjbGUgY3g9IjMyLjAwMDIiIGN5PSIyMS4zMzMyIiByPSIxMC42NjY3IiBmaWxsPSIjQTBBMEEyIi8+CjwvZz4KPC9zdmc+Cg=="
--- js/views/headerView.js
+++ js/views/headerView.js
@@ -1,54 +1,113 @@
+import * as User from "../models/userModel.js"
+
 function createHeader(){
   const header = document.createElement("header")
   header.className = "border-bottom"
-  header.innerHTML = `
-  <div class="header flex space-between items-center container mx-auto px-container">
-    <a href="../index.html"><img src="/img/LogoTSIW.svg"></a>
-    <nav class="flex items-center gap-32">
-        <ul class="header-links flex items-center gap-24">
-            <li><a href="" class="hover-underline-animation text-16 text-medium">Aprender</a></li>
-            <li><a href="/html/atividades.html" class="hover-underline-animation text-16 text-medium">Atividades</a></li>
-            <li><a href="/html/projetos.html" class="hover-underline-animation text-16 text-medium">Projetos</a></li>
-            <li><a href="/html/testemunhos.html" class="hover-underline-animation text-16 text-medium">Testemunhos</a></li>
-            <li><a href="" class="hover-underline-animation text-16 text-medium">Sobre</a></li>
-        </ul>
-        <a href="/html/registo.html" class="button" id="headerButtonRegisto" role="button">Registar</a>
-        <button id="buttonHamburger" class="header-hamburger flex direction-column">
-          <div class="hamburger-bar"></div>
-          <div class="hamburger-bar"></div>
-          <div class="hamburger-bar"></div>
-        </button>
-    </nav>
-  </div>
-  <div id="sideNavBar" class="side-nav h-screen flex direction-column gap-48">
-      <div class="flex space-between items-center">
-          <a href="../index.html"><img src="/img/LogoTSIW Versao pequena.svg" height="30px"></a>
-          <div class="flex gap-16 items-center">
-              <a href="/html/registo.html" class="button" role="button">Registar</a>
-              <button id="buttonSideNavClose" class="header-hamburger flex">
-                <img src="/img/cross.svg" height="12px">
-              </button>
+  if(User.isLogged()){
+    let loggedUser = User.isLogged()
+    header.innerHTML = `
+    <div class="header flex space-between items-center container mx-auto px-container">
+      <a href="../index.html"><img src="/img/LogoTSIW.svg"></a>
+      <nav class="flex items-center gap-32">
+          <ul class="header-links flex items-center gap-24">
+              <li><a href="" class="hover-underline-animation text-16 text-medium">Aprender</a></li>
+              <li><a href="/html/atividades.html" class="hover-underline-animation text-16 text-medium">Atividades</a></li>
+              <li><a href="/html/projetos.html" class="hover-underline-animation text-16 text-medium">Projetos</a></li>
+              <li><a href="/html/testemunhos.html" class="hover-underline-animation text-16 text-medium">Testemunhos</a></li>
+              <li><a href="" class="hover-underline-animation text-16 text-medium">Sobre</a></li>
+          </ul>
+          <div>
+            <a href="/html/notificacoes.html"><img src="img/NotificationIcon.svg" width="16px" height="18px"></a>
+            <a href="/html/conta.html"><img src="${loggedUser.avatar}" width="36px" height="36px"></a>
           </div>
-      </div>
-      <div class="flex direction-column gap-16">
-          <p class="text-18 text-medium text-mainGrey">Navegação</p>
-          <ul class="flex direction-column gap-16">
-              <li><a href="" class="hover-underline-animation text-18 text-medium">Aprender</a></li>
-              <li><a href="/html/atividades.html" class="hover-underline-animation text-18 text-medium">Atividades</a></li>
-              <li><a href="/html/projetos.html" class="hover-underline-animation text-18 text-medium">Projetos</a></li>
-              <li><a href="/html/testemunhos.html" class="hover-underline-animation text-18 text-medium">Testemunhos</a></li>
-              <li><a href="" class="hover-underline-animation text-18 text-medium">Sobre</a></li>
+          <button class="button" id="headerButtonLogOut">Log out</button>
+          <button id="buttonHamburger" class="header-hamburger flex direction-column">
+            <div class="hamburger-bar"></div>
+            <div class="hamburger-bar"></div>
+            <div class="hamburger-bar"></div>
+          </button>
+      </nav>
+    </div>
+    <div id="sideNavBar" class="side-nav h-screen flex direction-column gap-48">
+        <div class="flex space-between items-center">
+            <a href="../index.html"><img src="/img/LogoTSIW Versao pequena.svg" height="30px"></a>
+            <div class="flex gap-16 items-center">
+                <button class="button" id="sideNavButtonLogOut">Log out</button>
+                <button id="buttonSideNavClose" class="header-hamburger flex">
+                  <img src="/img/cross.svg" height="12px">
+                </button>
+            </div>
+        </div>
+        <div class="flex direction-column gap-16">
+            <p class="text-18 text-medium text-mainGrey">Navegação</p>
+            <ul class="flex direction-column gap-16">
+                <li><a href="" class="hover-underline-animation text-18 text-medium">Aprender</a></li>
+                <li><a href="/html/atividades.html" class="hover-underline-animation text-18 text-medium">Atividades</a></li>
+                <li><a href="/html/projetos.html" class="hover-underline-animation text-18 text-medium">Projetos</a></li>
+                <li><a href="/html/testemunhos.html" class="hover-underline-animation text-18 text-medium">Testemunhos</a></li>
+                <li><a href="" class="hover-underline-animation text-18 text-medium">Sobre</a></li>
+            </ul>
+        </div>
+        <div class="flex direction-column gap-16">
+            <p class="text-18 text-medium text-mainGrey">Redes Sociais</p>
+            <ul class="flex direction-column gap-16">
+                <li><a href="https://www.facebook.com/LTSIW" class="hover-underline-animation text-18 text-medium">Facebook</a></li>
+                <li><a href="https://www.instagram.com/_tsiw.esmad_/" class="hover-underline-animation text-18 text-medium">Instagram</a></li>
+                <li><a href="https://www.youtube.com/channel/UCA1uYDftSXjN1Lx5E9-ByIg" class="hover-underline-animation text-18 text-medium">Youtube</a></li>
+            </ul>
+        </div>
+    </div>`
+  }
+  else{
+    header.innerHTML = `
+    <div class="header flex space-between items-center container mx-auto px-container">
+      <a href="../index.html"><img src="/img/LogoTSIW.svg"></a>
+      <nav class="flex items-center gap-32">
+          <ul class="header-links flex items-center gap-24">
+              <li><a href="" class="hover-underline-animation text-16 text-medium">Aprender</a></li>
+              <li><a href="/html/atividades.html" class="hover-underline-animation text-16 text-medium">Atividades</a></li>
+              <li><a href="/html/projetos.html" class="hover-underline-animation text-16 text-medium">Projetos</a></li>
+              <li><a href="/html/testemunhos.html" class="hover-underline-animation text-16 text-medium">Testemunhos</a></li>
+              <li><a href="" class="hover-underline-animation text-16 text-medium">Sobre</a></li>
           </ul>
-      </div>
-      <div class="flex direction-column gap-16">
-          <p class="text-18 text-medium text-mainGrey">Redes Sociais</p>
-          <ul class="flex direction-column gap-16">
-              <li><a href="https://www.facebook.com/LTSIW" class="hover-underline-animation text-18 text-medium">Facebook</a></li>
-              <li><a href="https://www.instagram.com/_tsiw.esmad_/" class="hover-underline-animation text-18 text-medium">Instagram</a></li>
-              <li><a href="https://www.youtube.com/channel/UCA1uYDftSXjN1Lx5E9-ByIg" class="hover-underline-animation text-18 text-medium">Youtube</a></li>
-          </ul>
-      </div>
-  </div>`
+          <a href="/html/registo.html" class="button" id="headerButtonRegisto" role="button">Registar</a>
+          <button id="buttonHamburger" class="header-hamburger flex direction-column">
+            <div class="hamburger-bar"></div>
+            <div class="hamburger-bar"></div>
+            <div class="hamburger-bar"></div>
+          </button>
+      </nav>
+    </div>
+    <div id="sideNavBar" class="side-nav h-screen flex direction-column gap-48">
+        <div class="flex space-between items-center">
+            <a href="../index.html"><img src="/img/LogoTSIW Versao pequena.svg" height="30px"></a>
+            <div class="flex gap-16 items-center">
+                <a href="/html/registo.html" class="button" role="button">Registar</a>
+                <button id="buttonSideNavClose" class="header-hamburger flex">
+                  <img src="/img/cross.svg" height="12px">
+                </button>
+            </div>
+        </div>
+        <div class="flex direction-column gap-16">
+            <p class="text-18 text-medium text-mainGrey">Navegação</p>
+            <ul class="flex direction-column gap-16">
+                <li><a href="" class="hover-underline-animation text-18 text-medium">Aprender</a></li>
+                <li><a href="/html/atividades.html" class="hover-underline-animation text-18 text-medium">Atividades</a></li>
+                <li><a href="/html/projetos.html" class="hover-underline-animation text-18 text-medium">Projetos</a></li>
+                <li><a href="/html/testemunhos.html" class="hover-underline-animation text-18 text-medium">Testemunhos</a></li>
+                <li><a href="" class="hover-underline-animation text-18 text-medium">Sobre</a></li>
+            </ul>
+        </div>
+        <div class="flex direction-column gap-16">
+            <p class="text-18 text-medium text-mainGrey">Redes Sociais</p>
+            <ul class="flex direction-column gap-16">
+                <li><a href="https://www.facebook.com/LTSIW" class="hover-underline-animation text-18 text-medium">Facebook</a></li>
+                <li><a href="https://www.instagram.com/_tsiw.esmad_/" class="hover-underline-animation text-18 text-medium">Instagram</a></li>
+                <li><a href="https://www.youtube.com/channel/UCA1uYDftSXjN1Lx5E9-ByIg" class="hover-underline-animation text-18 text-medium">Youtube</a></li>
+            </ul>
+        </div>
+    </div>`
+  }
   
   const mainContainer = document.getElementById("mainContainer")
   mainContainer.prepend(header) 
