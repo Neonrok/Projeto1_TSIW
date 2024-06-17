5f66d027ebc36966527df3e5d2c8de8e87587d35
Xavier Kooijman <xavierkooijman@gmail.com>
2024-06-17 20:48:09 +0100

    projeto slider ao clicar nos botoes do slider ignorar event listner de ir para pagina de projeto. Check if admin is logging in

--- js/views/loginView.js
+++ js/views/loginView.js
@@ -9,6 +9,15 @@ const loginForm = document.getElementById("loginForm")
 loginForm.addEventListener("submit", (event)=>{
   event.preventDefault()
 
+  if(loginName.value === "admin"){
+    try {
+      User.login(loginName.value,loginPassword.value)
+      location.href = "/html/admin.html"
+    } catch (e) {
+      loginForm.reset()
+    }
+  }
+
   try {
     User.login(loginName.value,loginPassword.value)
     location.href = "/html/conta.html"
